fd419c59441f43f3b3e4337f115f2d5a7a749cc5
Xavier <oncwnuCkaVoD50F26aK1rQmM2L8U@git.weixin.qq.com>
2022-07-28 23:51:14 +0800

    Chapter 5

---
+++ src/main/java/org/xavier/redis10stream/Group.java
@@ -0,0 +1,54 @@
+package org.xavier.redis10stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import redis.clients.jedis.*;
+
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class Group {
+    private Jedis client;
+    private String streamKey;
+    private String groupId;
+
+    //创建消费者组
+    public void createGroup(String startId){
+        this.client.xgroupCreate(streamKey, groupId, new StreamEntryID(startId), false);
+    }
+
+    //删除消费者组
+    public void destroyGroup(){
+        this.client.xgroupDestroy(streamKey, groupId);
+    }
+
+    //从消费者组中读取信息
+    public List<Map.Entry<String, List<StreamEntry>>> readMessage(String consumerId, int count) {
+        Map.Entry<String, StreamEntryID> entry =
+                new AbstractMap.SimpleImmutableEntry<>(streamKey, StreamEntryID.UNRECEIVED_ENTRY);
+        return this.client.xreadGroup(groupId, consumerId, count, 0, true, entry);
+    }
+
+    //确认已处理完毕的信息
+    public Long ackMessage(String messageId){
+        return this.client.xack(streamKey, groupId, new StreamEntryID(messageId));
+    }
+
+    //返回消费组信息
+    public List<StreamGroupInfo> info() {
+        return this.client.xinfoGroup(streamKey);
+    }
+
+    //返回消费者信息
+    public List<StreamConsumersInfo> consumerInfo() {
+        return this.client.xinfoConsumers(streamKey, groupId);
+    }
+
+    //删除指定的消费者
+    public void deleteConsumer(String consumerId) {
+        this.client.xgroupDelConsumer(streamKey, groupId, consumerId);
+    }
+}
---
+++ src/test/java/org/xavier/redis10stream/GroupTest.java
@@ -0,0 +1,108 @@
+package org.xavier.redis10stream;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+@Slf4j
+public class GroupTest {
+    private Group group;
+
+    @Before
+    public void setUp() throws Exception {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        //FlushDB
+        jedis.flushDB();
+        //Create Stream
+        Map<String, String> member1 = new HashMap<>();
+        member1.put("k1", "v1");
+        Map<String, String> member2 = new HashMap<>();
+        member2.put("k2", "v2");
+        Map<String, String> member3 = new HashMap<>();
+        member3.put("k3", "v3");
+        Map<String, String> member4 = new HashMap<>();
+        member4.put("k4", "v4");
+        jedis.xadd("s1", null, member1);
+        jedis.xadd("s1", null, member2);
+        jedis.xadd("s1", null, member3);
+        jedis.xadd("s1", null, member4);
+        group = new Group(jedis, "s1", "group1");
+    }
+
+    @Test
+    public void createGroup() {
+        group.createGroup("0-0");
+    }
+
+    @Test
+    public void destroyGroup() {
+        group.destroyGroup();
+    }
+
+    @Test
+    public void readMessage() {
+        group.createGroup("0-0");
+        List<Map.Entry<String, List<StreamEntry>>> result = group.readMessage("c1", 2);
+        Optional.ofNullable(result).ifPresent(list -> {
+            log.info(String.valueOf(list.size()));
+            if(list.size() == 1) {
+                list.get(0).getValue().forEach(se->{
+                    log.info(se.getID()+"\t"+se.getFields().toString());
+                });
+            }
+        });
+    }
+
+    @Test
+    public void ackMessage() {
+        group.createGroup("0-0");
+        List<Map.Entry<String, List<StreamEntry>>> result = group.readMessage("c1", 1);
+        Optional.ofNullable(result).ifPresent(list -> {
+            log.info(String.valueOf(list.size()));
+            if(list.size() == 1) {
+                list.get(0).getValue().forEach(se->{
+                    String messageId = se.getID().toString();
+                    //Ack Message
+                    Long ackNumber = group.ackMessage(messageId);
+                    Optional.ofNullable(ackNumber).ifPresent(ack->{
+                        log.info("Acknowledge Message");
+                    });
+                });
+            }
+        });
+
+    }
+
+    @Test
+    public void info() {
+        group.createGroup("0-0");
+        List<StreamGroupInfo> groupInfo = group.info();
+        Optional.ofNullable(groupInfo).ifPresent(gi ->  {
+            log.info(gi.toString());
+        });
+    }
+
+    @Test
+    public void consumerInfo() {
+        group.createGroup("0-0");
+        List<Map.Entry<String, List<StreamEntry>>> result = group.readMessage("c1", 1);
+        List<StreamConsumersInfo> consumerInfo = group.consumerInfo();
+        Optional.ofNullable(consumerInfo).ifPresent(ci ->  {
+            log.info(ci.toString());
+        });
+    }
+
+    @Test
+    public void deleteConsumer() {
+        group.createGroup("0-0");
+        List<Map.Entry<String, List<StreamEntry>>> result = group.readMessage("c1", 1);
+        group.deleteConsumer("c1");
+    }
+}
